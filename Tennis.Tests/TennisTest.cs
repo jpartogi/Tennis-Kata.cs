437a312e79e54cdeaea8499eaf3f1c48e529668d
Joshua Partogi <proscrumtrainer@live.com>
2016-05-27 22:17:33 +0800

    Initial commit

---
+++ Tennis.Tests/TennisTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Tennis
+{
+    using NUnit.Framework;
+    
+    public class TennisTest
+    {
+        private Game game;
+        private const string ERIC = "Eric";
+        private const string JOHN = "John";
+
+        [SetUp]
+        public void SetUp()
+        {
+            game = new TennisGame(ERIC, JOHN);
+        }
+
+    }
+}
---
+++ Tennis/Game.cs
@@ -0,0 +1,8 @@
+﻿namespace Tennis
+{
+    public interface Game
+    {
+        void WonPoint(string playerName);
+        string GetScore();
+    }
+}
